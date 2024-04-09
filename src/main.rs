a897b6db204857786a15a1ab428d3eb2bac6f357
Hiury Joaquim Oliveira André Araújo <hiuryo1996@gmail.com>
2024-04-09 20:04:06 -0300

    Adicionando implementação do Bubble Sort simples.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sortable = [64, 34, 25, 12, -18, 22, 11, 18, 26, 69, 9, 90];
+    sortable::bubble_sort(&mut sortable);
+    println!("Sorted: {:?}", sortable);
+}
+
+mod sortable {
+    pub(crate) fn bubble_sort<T: Ord>(sortable: &mut [T]) {
+        let n = sortable.len();
+        for i in 0..n {
+            for j in 0..n - i - 1 {
+                if sortable[j] > sortable[j + 1] {
+                    sortable.swap(j, j + 1);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut sortable = [64, 34, 25, 12, -18, 22, 11, 18, 26, 69, 9, 90];
+        sortable::bubble_sort(&mut sortable);
+        assert_eq!(sortable, [-18, 9, 11, 12, 18, 22, 25, 26, 34, 64, 69, 90]);
+    }
 }
